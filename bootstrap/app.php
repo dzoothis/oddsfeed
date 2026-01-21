4bfdef7977f264372ff6b7be1c9617ebc30399a0
alpesh.aipxperts <alpesh.lalakiya@aipxperts.com>
2026-01-21 11:04:12 +0530

    changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -16,4 +16,20 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+/*
+|--------------------------------------------------------------------------
+| Bind Console Kernel
+|--------------------------------------------------------------------------
+| Laravel 12 requires explicit binding in some setups to ensure
+| the Console Kernel (scheduler, commands) is recognized.
+|--------------------------------------------------------------------------
+*/
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+return $app;
